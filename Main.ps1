b2463370eaefefc31d11848d0e8d0ceee876c471
Elfry <elfry3@gmail.com>
2025-08-04 18:57:18 +0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -4,15 +4,9 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;
 iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 # Install the required Chocolatey packages.
-choco install -y postgresql
-choco install -y heidisql
 choco install -y nginx --version=1.29.0
 choco install -y php --version=8.4.8
-choco install -y composer
-choco install -y mongodb
-choco install -y mongodb-compass
-choco install -y nodejs
-choco install -y neovim
+choco install -y postgresql heidisql composer mongodb mongodb-compass nodejs neovim
 
 # Copy the nginx.conf to the installation directory.
 Copy-Item ".\nginx.conf" -Destination "C:\tools\nginx-1.29.0\conf\"
