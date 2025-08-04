f60d964057559b174fa2be74716398f490e3de2d
Elfry <elfry3@gmail.com>
2025-08-05 00:15:54 +0700

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -10,9 +10,9 @@ scoop bucket add versions
 scoop bucket add extras
 
 # Install the required Scoop packages.
-scoop install postgresql heidisql nginx@1.29.0 php@8.4.8 composer nodejs mongodb mongodb-compass curlie neovim
+scoop install postgresql heidisql nginx@1.29.0 php82 composer nodejs mongodb mongodb-compass curlie neovim
 
 # Copy the files into the installation directories.
 # Inspired by something found on https://gist.github.com/odan/b5f7de8dfbdbf76bef089776c868fea1.
-Copy-Item ".\nginx.conf" -Destination "$HOME\AppData\scoop\nginx1.29.0\conf\" -Verbose 
-Copy-Item ".\phpinfo.php" -Destination "$HOME\AppData\scoop\nginx1.29.0\html\" -Verbose 
+Copy-Item ".\nginx.conf" -Destination "$HOME\AppData\scoop\nginx1.29.0\conf\" -Verbose
+Copy-Item ".\phpinfo.php" -Destination "$HOME\AppData\scoop\nginx1.29.0\html\" -Verbose
