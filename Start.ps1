548466ca4871916c1b033200ba399d88f5fcf868
Elfry <elfry3@gmail.com>
2025-08-04 23:49:10 +0700

    Create Start.ps1

---
+++ Start.ps1
@@ -0,0 +1,10 @@
+# PHP-CGI
+cd $HOME\AppData\scoop\php82
+Start-Process -NoNewWindow php-cgi.exe -b 127.0.0.1:9123
+
+# nginx
+cd $HOME\AppData\scoop\nginx1.29.0
+start nginx
+
+# PostgreSQL
+pg_ctl start
