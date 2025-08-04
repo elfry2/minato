2a716ef6301f090f5dcea4d4eece9f48fb82c5de
Elfry <elfry3@gmail.com>
2025-08-04 20:31:20 +0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -6,7 +6,8 @@ iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocola
 # Install the required Chocolatey packages.
 choco install --yes nginx --version=1.29.0
 choco install --yes php --version=8.4.8
-choco install --yes postgresql17 heidisql composer mongodb mongodb-compass nodejs neovim
+choco install --yes postgresql17 --params '/Password:test'
+choco install --yes heidisql composer mongodb mongodb-compass nodejs neovim
 
 # Copy the nginx.conf to the installation directory.
 # Inspired by something found on https://gist.github.com/odan/b5f7de8dfbdbf76bef089776c868fea1.
