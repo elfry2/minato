00918dbe2ffcb2940379edda561d06ff95378c1e
Elfry <elfry3@gmail.com>
2025-08-04 16:25:59 +0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -3,4 +3,4 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 
 # Install the required Scoop packages
-scoop install git nginx mariadb php nodejs mongodb lazyvim
+scoop install git postgresql nginx php composer nodejs mongodb neovim
