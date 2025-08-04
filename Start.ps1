96a1eb7bb192c76a73004e156c5be81e845bd218
Elfry <elfry3@gmail.com>
2025-08-05 00:49:49 +0700

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,11 +1,11 @@
 $InitialDirectory = "$CWD"
 
 # Start PHP-CGI.
-cd $HOME\AppData\scoop\php82
+cd $HOME\AppData\scoop\php
 Start-Process -NoNewWindow php-cgi.exe -b 127.0.0.1:9123
 
 # Start nginx.
-cd $HOME\AppData\scoop\nginx1.29.0
+cd $HOME\AppData\scoop\nginx-1.29.0
 start nginx
 
 # Start PostgreSQL.
