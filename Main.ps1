91b616422a05feb2f215edb5d8c42cd48ee20637
Elfry <elfry3@gmail.com>
2025-08-04 16:57:43 +0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,15 +1,9 @@
-# Install Scoop.
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Out-Null
+# Install Chocolatey.
+Set-ExecutionPolicy Bypass -Scope Process -Force;
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072;
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
-# Install git via Scoop.
-scoop install git
+# Install the required Chocolatey packages.
+choco install postgresql heidisql nginx php composer mongodb nodejs neovim
 
-# Install the required Scoop buckets.
-scoop bucket add extras
-
-# Install the required Scoop packages
-scoop install postgresql heidisql nginx@1.29.0 php composer nodejs mongodb neovim
-
-# Copy the nginx.conf.
-Copy-Item "nginx.conf" -Destination "$HOME\scoop\apps\nginx\1.29.0\conf"
+# Copy the nginx.conf to the installation directory.
