700fab02bed74b9a9af87619c6b3538411afb5f5
Elfry <elfry3@gmail.com>
2025-08-04 16:33:00 +0700

    Update Main.ps1

--- Main.ps1
+++ Main.ps1
@@ -1,6 +1,6 @@
 # Install Scoop.
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
-Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+Invoke-RestMethod -Uri https://get.scoop.sh | Out-Null
 
 # Install the required Scoop packages
 scoop install git postgresql nginx php composer nodejs mongodb neovim
