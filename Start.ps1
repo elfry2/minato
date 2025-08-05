404f5ad55e837c8a8275d820742e6c575121af84
Elfry <elfry3@gmail.com>
2025-08-05 09:32:31 +0700

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -2,7 +2,7 @@ $InitialDirectory = "$CWD"
 
 # Start PHP-CGI.
 cd $HOME\scoop\apps\php82\8.2.29
-Start-Process -NoNewWindow php-cgi.exe -b 127.0.0.1:9123
+Start-Process -NoNewWindow { php-cgi.exe -b 127.0.0.1:9123 }
 
 # Start nginx.
 cd $HOME\scoop\apps\nginx\1.29.0
