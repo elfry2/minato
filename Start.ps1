02221de7bec8816dd20da9d8e8d6f9dbcf440e3e
Elfry <elfry3@gmail.com>
2025-08-05 09:42:21 +0700

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,13 +1,8 @@
-$InitialDirectory = "$PWD"
-
 # Start PHP-CGI.
 start-job {php-cgi.exe -b 127.0.0.1:9123}
 
 # Start nginx.
-cd $HOME\scoop\apps\nginx\1.29.0
 start nginx
 
 # Start PostgreSQL.
 pg_ctl start
-
-cd $InitialDirectory
