66895f656ccf1d466141a23d55ce447f86cca99a
Elfry <elfry3@gmail.com>
2025-08-05 09:18:49 +0700

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -3,13 +3,13 @@ Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
 Invoke-RestMethod -Uri https://get.scoop.sh | Out-Null
 
 # Install Git via Scoop.
-scoop install git
+scoop install git -ErrorAction "Resume"
 
 # Install the required Scoop buckets.
-scoop bucket add extras
+scoop bucket add extras -ErrorAction "Resume"
 
 # Install the required Scoop packages.
-scoop install postgresql dbeaver nginx@1.29.0 php@8.2.0 composer nodejs mongodb mongodb-compass curlie neovim
+scoop install postgresql dbeaver nginx@1.29.0 php@8.2.0 composer nodejs mongodb mongodb-compass curlie neovim -ErrorAction "Resume"
 
 # Copy the files into the installation directories.
 # Inspired by something found on https://gist.github.com/odan/b5f7de8dfbdbf76bef089776c868fea1.
