469c7c0fd064e55cde7b20874000795757cb213c
Elfry <elfry3@gmail.com>
2025-08-05 09:56:53 +0700

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -4,8 +4,7 @@ $InitialDirectory = "$PWD"
 start-job {php-cgi.exe -b 127.0.0.1:9123}
 
 # Start nginx.
-cd "$HOME\scoop\apps\nginx\1.29.0"
-start nginx
+start-job {cd "$HOME\scoop\apps\nginx\1.29.0"; start nginx}
 
 # Start PostgreSQL.
 pg_ctl start
