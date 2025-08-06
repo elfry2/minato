407c77a04237a68f54fae5b5301e2beebed3a132
Elfry <elfry3@gmail.com>
2025-08-06 18:29:41 +0700

    Update Install.ps1

--- Install.ps1
+++ Install.ps1
@@ -12,7 +12,7 @@ scoop bucket add versions
 # Install the required Scoop packages.
 scoop install nginx@1.29.0
 scoop install php82@8.2.29
-scoop install postgresql composer nodejs mongodb mongosh curlie neovim gh
+scoop install nssm postgresql composer nodejs mongodb mongosh curlie neovim gh
 
 # Copy the files into the installation directories.
 # Inspired by something found on https://gist.github.com/odan/b5f7de8dfbdbf76bef089776c868fea1.
