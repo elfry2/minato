07d1689da10b4dcd4ffe1603622ad028fd770496
Elfry <elfry3@gmail.com>
2025-08-07 18:22:38 +0700

    Update Start.ps1

--- Start.ps1
+++ Start.ps1
@@ -1,12 +1,12 @@
 $InitialDirectory = "$PWD"
 
 # Start PHP-CGI.
-Start-Process { php-cgi.exe -b 127.0.0.1:9123 } -WindowStyle hidden
+Start-Process powershell { php-cgi.exe -b 127.0.0.1:9123 } -WindowStyle hidden
 
 # Start nginx.
-Start-Process { cd $HOME\scoop\apps\nginx\1.29.0; start .\nginx } -WindowStyle hidden
+Start-Process powershell { cd $HOME\scoop\apps\nginx\1.29.0; start .\nginx } -WindowStyle hidden
 
 # Start PostgreSQL.
-Start-Process { pg_ctl start } -WindowStyle hidden
+Start-Process powershell { pg_ctl start } -WindowStyle hidden
 
 cd $InitialDirectory
