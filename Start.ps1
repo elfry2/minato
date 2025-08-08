7915f4ffbac2a50a8185d26e5b5cf7a3cb1decd4
Elfry <elfry3@gmail.com>
2025-08-08 16:29:32 +0700

    Load config in the start script

--- Start.ps1
+++ Start.ps1
@@ -3,12 +3,16 @@
 $InitialDirectory = "$PWD"
 
 # Start PHP-CGI.
-Start-Process powershell { php-cgi.exe -b 127.0.0.1:9123 } -WindowStyle hidden
+Start-Process powershell { php-cgi.exe -b 127.0.0.1:9123 } -Verbose -WindowStyle hidden
 
 # Start nginx.
-Start-Process powershell { cd "$HOME\scoop\apps\nginx\$NginxVersion"; start .\nginx } -WindowStyle hidden
+Start-Process powershell {
+  . C:\shinra\Config.ps1;
+  cd "$HOME\scoop\apps\nginx\$NginxVersion";
+  start .\nginx
+} -Verbose -WindowStyle hidden
 
 # Start PostgreSQL.
-Start-Process powershell { pg_ctl start } -WindowStyle hidden
+Start-Process powershell { pg_ctl start } -Verbose -WindowStyle hidden
 
 cd $InitialDirectory
